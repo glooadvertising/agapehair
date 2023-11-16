3de55b79a50a97f4454122a11273f14e54654547
Pablo Sazo <pablo@glooadvertising.com.au>
2023-11-16 16:56:51 +1100

    oki doki

---
+++ partials/chat.php
@@ -0,0 +1,17 @@
+<chat-widget
+  location-id="Stn4jwf5y0fOBtlAlwIJ"
+  style="--chat-widget-primary-color: #D5BEDC; --chat-widget-active-color:#D5BEDC ;--chat-widget-bubble-color: #D5BEDC ;"
+  prompt-msg="Hey, how can we help you today?" 
+  legal-msg="By submitting you agree to receive communication for the provided channel. " 
+  success-msg="We will contact you shortly 😃 " 
+  thank-you-msg="Thank You! " 
+  prompt-avatar="https://firebasestorage.googleapis.com/v0/b/highlevel-backend.appspot.com/o/locationPhotos%2FStn4jwf5y0fOBtlAlwIJ%2Fchat-widget-person?alt=media&token=912dd620-fe5e-4343-8df7-f54e5df8fe55" 
+  agency-name="ZOCCO" 
+  agency-website="https://zocco.com.au/" 
+  locale="en-gb" 
+  primary-color="#D5BEDC">
+</chat-widget>
+ <script
+  src="https://widgets.leadconnectorhq.com/loader.js" 
+  data-resources-url="https://widgets.leadconnectorhq.com/chat-widget/loader.js" >
+ </script>
