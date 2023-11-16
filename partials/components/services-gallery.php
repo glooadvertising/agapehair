cbb4b5b8c0942b20c1390e5f29e09ee7961927ca
Pablo Sazo <pablo@glooadvertising.com.au>
2023-11-17 09:42:10 +1100

    Archie fixed

--- partials/components/services-gallery.php
+++ partials/components/services-gallery.php
@@ -1,5 +1,5 @@
 <section class="services-gallery">
     <img src="<?php echo get_template_directory_uri()?>/assets/images/agape-1.jpg" alt="">
-    <img src="<?php echo get_template_directory_uri()?>/assets/images/agape-4.jpg" alt="">
     <img src="<?php echo get_template_directory_uri()?>/assets/images/our-services_bg.jpg" alt="">
+    <img src="<?php echo get_template_directory_uri()?>/assets/images/agape-4.jpg" alt="">
 </section>
